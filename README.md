81699adc261c2b8c7ff69f6a73cdb919ae5d35ef
F-Hameau <f.hameau@gmail.com>
2024-12-09 20:06:46 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # execute-salesforce
+
 A nvim package to execute anonymous apex or soql
+
+# Votre Plugin Neovim
+
+Ce plugin permet d'exécuter du code Apex directement depuis Neovim.
+
+## Installation
+
+Utilisez Lazy.nvim pour installer ce plugin.
+
+```lua
+-- Exemple de configuration avec Lazy.nvim
+use {
+  'F-Hameau/execute-salesforce',
+  cmd = 'ExecuteApex', -- Charger à la commande `:ExécuterApex`
+  config = function()
+    require('./lua/apex').setup()  -- Optionnel si vous avez une fonction de setup
+  end,
+}
+
+```
---
+++ lua/apex/init.lua
@@ -0,0 +1,45 @@
+local M = {}
+local telescope = require("telescope")
+
+function M.execute_anonymous()
+	local code = vim.fn.getreg('"')
+	if code == "" then
+		print("Aucun code à exécuter.")
+		return
+	end
+
+	local command = 'sfdx force:apex:execute --apexcode "' .. code .. '"'
+	local handle = io.popen(command)
+	local result = handle:read("*a")
+	handle:close()
+
+	M.show_result(result)
+end
+
+function M.show_result(result)
+	-- Préparation des données pour l'affichage
+	local lines = {}
+	for line in result:gmatch("([^\r\n]*)\r?\n?") do
+		table.insert(lines, line)
+	end
+
+	-- Affichage avec telescope
+	telescope.pickers
+		.new({}, {
+			prompt_title = "Apex Execution Result",
+			finder = telescope.finders.new_table({
+				results = lines,
+			}),
+			sorter = telescope.sorters.get_generic_fuzzy_sorter(),
+			previewer = telescope.previewers.new_buffer_previewer({
+				define_preview = function(self, entry, _status)
+					vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { entry.value })
+				end,
+			}),
+		})
+		:find()
+end
+
+vim.api.nvim_create_user_command("ExecuteApex", M.execute_anonymous, {})
+
+return M
